ba4b58dedc91e8c94c62e1f4a8a4c59dfb443dc4
vale@luna <vthvw02@gmail.com>
2025-11-30 14:46:12 +0100

    add header for SubdomainSolver

---
+++ src/solver.cpp
@@ -0,0 +1,11 @@
+#include <solver.hpp>
+
+SubdomainSolver::SubdomainSolver(const PDEPArams &pdep, const SchwarzParams &sp, BoundaryVals &bv, const Real h, const Index i)
+        : mu(pdep.mu), c(pdep.c), f(pdep.f), omega(pdep.omega),
+          Nsub(sp.N), delta(sp.delta),
+          boundary_values(bv),
+          h(h),
+          i(i)
+    {
+        //TODO initialize FactorizedTridiag
+    }
