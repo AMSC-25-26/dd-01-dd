6717bacefc75f95d206d73d598f841b36d7bcf70
Alessia Rigoni <a.rigoni5@campus.unimib.it>
2025-11-30 15:19:56 +0100

    Add types.hpp

--- include/types.hpp
+++ include/types.hpp
@@ -2,8 +2,42 @@
 #include <functional>
 #include <cstddef>
 
-using Real = double;
-using Index = int;
-using Size = size_t;
-using Vector = std::vector<Real>;
-using Function = std::function<Real(Real)>;
+struct Types {
+    using Real = double;
+    using Index = int;
+    using Size = size_t;
+    using Vector = std::vector<Real>;
+    using Function = std::function<Real(Real)>;
+
+    // when we add 2 dimensions, we can add the other things 
+    template<int dim>
+    struct Domain {
+        Real a, b;
+    };
+
+    template<int dim>
+    struct BoundaryVals {
+        Real u_a, u_b;
+    };
+
+    struct SubIndexes {
+        Index i, j;
+    };
+
+    template<int dim>
+    struct PDEParams {
+        Real mu, c;
+        Function f;
+        Domain<dim> omega;
+    };
+
+    struct SchwarzParams {
+        int N;
+        Real delta;
+    };
+
+    struct SolverParams {
+        Real eps;
+        int maxiter;
+    };
+}
