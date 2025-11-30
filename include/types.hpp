18ef2bfd0c6303c1f76881d9d350886ea574d3f3
Valeriia <valery_potrebina_2003@mail.ru>
2025-11-30 15:04:20 +0300

    Add types

---
+++ include/types.hpp
@@ -0,0 +1,9 @@
+#include <vector>
+#include <functional>
+#include <cstddef>
+
+using Real = double;
+using Index = int;
+using Size = size_t;
+using Vector = std::vector<Real>;
+using Function = std::function<Real(Real)>;
