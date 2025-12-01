c39d62046412d2aca12708bd0c38c23b947321b6
vale@luna <vthvw02@gmail.com>
2025-12-01 11:35:56 +0100

    added initial subdomain solver to solver cpp file

--- src/solver.cpp
+++ src/solver.cpp
@@ -1,3 +1,11 @@
-//
-// Created by vale on 12/1/25.
-//
+#include <solver.hpp>
+
+SubdomainSolver::SubdomainSolver(const PDEPArams &pdep, const SchwarzParams &sp, BoundaryVals &bv, const Real h, const Index i)
+        : mu(pdep.mu), c(pdep.c), f(pdep.f), omega(pdep.omega),
+          Nsub(sp.N), delta(sp.delta),
+          boundary_values(bv),
+          h(h),
+          i(i)
+{
+    //TODO initialize FactorizedTridiag
+}
