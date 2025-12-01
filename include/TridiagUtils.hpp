9bca0f414b37b1c4991e830ea17e0030d128ee99
Valeriia <valery_potrebina_2003@mail.ru>
2025-12-01 11:09:47 +0300

    Add Types inheritance

--- include/TridiagUtils.hpp
+++ include/TridiagUtils.hpp
@@ -1,19 +1,19 @@
-#include "types.hpp"
+#include <types.hpp>
 #include <iostream>
 
-class FactorizedTridiag {
+class FactorizedTridiag  : protected Types{
 private:
-    Types::Vector lower_;  // n-1 elements: l[0] ... l[n-2]
-    Types::Vector diag_;   // n elements:   d[0] ... d[n-1]
-    Types::Vector upper_;  // n-1 elements: u[0] ... u[n-2]
-    Types::Size n_;
+    Vector lower_;  // n-1 elements: l[0] ... l[n-2]
+    Vector diag_;   // n elements:   d[0] ... d[n-1]
+    Vector upper_;  // n-1 elements: u[0] ... u[n-2]
+    Size n_;
 
 public:
-    FactorizedTridiag(Types::Size n);
+    FactorizedTridiag(Size n);
 
-    Types::Real& operator()(Types::Index i, Types::Index j);
+    Real& operator()(Index i, Index j);
 
-    Types::Vector solve(const Types::Vector& b);
+    Vector solve(const Vector& b);
 
     void print() const;
 };
