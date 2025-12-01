8d8c2a03f23d3d899372b17faed25ba5364afc7a
vale@luna <vthvw02@gmail.com>
2025-12-01 13:05:20 +0100

    Adjusted solver.hpp and solver.cpp to account for new template organization

--- src/solver.cpp
+++ src/solver.cpp
@@ -1,11 +1,18 @@
 #include <solver.hpp>
 
-SubdomainSolver::SubdomainSolver(const PDEPArams &pdep, const SchwarzParams &sp, BoundaryVals &bv, const Real h, const Index i)
-        : mu(pdep.mu), c(pdep.c), f(pdep.f), omega(pdep.omega),
-          Nsub(sp.N), delta(sp.delta),
-          boundary_values(bv),
-          h(h),
-          i(i)
-{
-    //TODO initialize FactorizedTridiag
+#include "types.hpp"
+
+PDESolver<1>::PDESolver(const PDEParams &pde_params, const SchwarzParams &schwarz_params,
+                        const SolverParams &solver_params, Real h) :
+            mu(pde_params.mu),c(pde_params.c),eps(solver_params.eps), delta(schwarz_params.delta),
+            h(h),omega(pde_params.omega),
+            f(pde_params.f),max_iter(solver_params.max_iter),
+            Nsub(schwarz_params.N)
+{}
+
+/** TODO change definition of PDESolver and instantiate normally */
+SubdomainSolver<1>::SubdomainSolver(const PDEParams &pdep, const SchwarzParams &sp, BoundaryVals *bv, const Real h,
+                                    const Index i) : PDESolver<1>(pdep,sp, {0.0,0},h), i(i), boundary_values(bv) {
+    /** TODO actually create constructor */
+    ftd = nullptr;
 }
