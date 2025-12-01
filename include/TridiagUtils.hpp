61358644b47122c48608fac1e038cd6f9137c85a
Valeriia <valery_potrebina_2003@mail.ru>
2025-12-01 11:13:10 +0300

    Rename variables

--- include/TridiagUtils.hpp
+++ include/TridiagUtils.hpp
@@ -3,10 +3,10 @@
 
 class FactorizedTridiag  : protected Types{
 private:
-    Vector lower_;  // n-1 elements: l[0] ... l[n-2]
-    Vector diag_;   // n elements:   d[0] ... d[n-1]
-    Vector upper_;  // n-1 elements: u[0] ... u[n-2]
-    Size n_;
+    Vector _lower;  // n-1 elements: l[0] ... l[n-2]
+    Vector _diag;   // n elements:   d[0] ... d[n-1]
+    Vector _upper;  // n-1 elements: u[0] ... u[n-2]
+    Size _n;
 
 public:
     FactorizedTridiag(Size n);
