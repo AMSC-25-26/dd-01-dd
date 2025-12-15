05e6998175356f96094d43b83185244a5bea4852
vale@luna <vthvw02@gmail.com>
2025-12-15 17:31:38 +0100

    added comments to solver.cpp

--- include/solver.hpp
+++ include/solver.hpp
@@ -17,35 +17,128 @@
 
 template<Dimension dim> class PDESolver : public Types<dim>{};
 
+/**
+ * @class PDESolver
+ * @brief An "abstract" class, not meant to be initialized
+ * This class is not meant to be initializable and only hosts
+ * functions and parameters that are shared by SubdomainSolver and DiscreteSolver
+ * @see SubdomainSolver
+ * @see DiscreteSolver
+ */
 template<> class PDESolver<Line> : public Types<Line> {
 
 public:
-    Real mu, c, delta, h;
+    /**
+     * @brief a real number strictly greater than 0.0
+     * The diffusion parameter of a Elliptic PDE it must be \f$\mu > 0\f$
+     */
+    Real mu;
+    /**
+     * @brief a real number greater or equal 0.0
+     * The reaction parameter of a Elliptic PDE it must be \f$c >= 0\f$
+     */
+    Real c;
+    /**
+     * @brief a real number strictly greater than 0.0
+     * The overlap of the Schwarz subdomains, such that, in 1D
+     * \f$b_i-a_{i+1}=\delta \foralli : 0<i<N_{subdomains}+1\f$
+     */
+    Real delta;
+    /**
+     * @brief a real number strictly greater than 0.0
+     * The distance between consecutive nodes in the grid, must be strictly positive
+     * and must divide the interval \f$\lefta,b\right\f$ evenly.
+     */
+    Real h;
+    /**
+     * @brief a domain on which the PDE is defined
+     * In 1D is a set of 2 real numbers (a and b)
+     * representing interval \f$\lefta,b\right\f$
+     * @see Types
+     */
     Domain omega;
+    /**
+     * @brief the boundary conditions of the PDE
+     * In 1D is a set of 2 real numbers representing the
+     * values of the solution at the boundary of omega
+     * @see Types
+     */
     BoundaryVals dirichlet;
+    /**
+     * @brief a function on the domain omega
+     * A callable object. It is imposed that \f$\calLu(x) = f(x) \forall x \in \Omega\f$.
+     * It is necessary for f to be defined for every x in omega
+     */
     Function f;
+    /**
+     * @brief the number of subdomains
+     * A strictly positive integer representing the number of subdomains
+     * that omega will be divided into.
+     */
     Size Nsub;
 
-    Boundary get_subdomain_nonoverlapping_boundary(Index i) const;
-    Boundary get_subdomain_overlapping_boundary(Index i) const;
-    Index get_leftmost_node(Boundary boundary) const;
-    Index get_rightmost_node(Boundary boundary) const;
-    Index get_number_of_contained_nodes(Boundary boundary) const;
 
-    /** TODO Maybe remove constructor */
     PDESolver(const PDEParams& pde_params,
               const SchwarzParams& schwarz_params,
-              Real h);
+              const Real h);
     ~PDESolver() = default;
 
 
     protected:
-    Size Nnodes;
-    Real subdomain_area, domain_area;
+    Size Nnodes;                    //*< @brief the number of total nodes
+    Real subdomain_area;            //*< @brief area of each nonoverlapping Schwarz subdomain
+    Real domain_area;               //*< @brief the total area of the domain
 
-    SubIndexes global_to_sub(Index k) const noexcept;
+    /**
+     * Given the index of a subdomain (starting from 0 to Nsub-1)
+     * computes the position of \f$lefta_i,b_i\right\f$
+     * @param i the index of the subdomain
+     * @return the boundary of the selected subdomain
+     */
+    Boundary get_subdomain_nonoverlapping_boundary(Index i) const;
+
+    /**
+     * Given the index of a subdomain (starting from 0 to Nsub-1)
+     * computes the position of \f$lefta_i-\frac{\delta}{2},b_i+\frac{\delta}{2}\right\f$
+     * @param i the index of the subdomain
+     * @return the boundary of the selected subdomain
+     */
+    Boundary get_subdomain_overlapping_boundary(Index i) const;
+
+    /**
+     * Given a boundary returns the global index of the leftmost node inside it
+     * @param boundary the boundary to examine
+     * @return the index of the leftmost node
+     */
+    Index get_leftmost_node(Boundary boundary) const;
+
+    /**
+     * Given a boundary returns the global index of the rightmost node inside it
+     * @param boundary the boundary to examine
+     * @return the index of the rightmost node
+     */
+    Index get_rightmost_node(Boundary boundary) const;
+    /**
+     * Given a boundary returns the number of nodes inside it
+     * @param boundary the boundary to examine
+     * @return the nuber of nodes in the boundary
+     */
+    int get_number_of_contained_nodes(Boundary boundary) const;
+
+    /**
+     * Given a set if indices i and j, compute the global index of the
+     * j-th node inside the i-th subdomain.
+     * @param ij the pair of indices
+     * @return the global index
+     */
     Index sub_to_local(SubIndexes ij) const noexcept;
-    Size nodes_in_subdomain(Index i) const noexcept;
+
+    /**
+     * Given a subdomain compute the number of nodes inside it
+     * @param i the index of the subdomain to be examined
+     * @return the number of nodes in the subdomain
+     */
+    int nodes_in_subdomain(Index i) const noexcept;
 };
 
 
@@ -69,7 +162,7 @@ template<> class SubdomainSolver<Line> : public PDESolver<Line> {
          * @see SubdomainSolver::update_boundary
          */
         BoundaryVals boundary_values;
-        Vector b;
+        Vector b;                           //*< @brief the right end side of the algebaric system
         FactorizedTridiag ftd;              //*< @see FactorizedTridiag
 
         SubdomainSolver(const PDEParams &pdep, const SchwarzParams &sp, BoundaryVals bv, const Real h, const Index i);
@@ -98,7 +191,8 @@ template<> class SubdomainSolver<Line> : public PDESolver<Line> {
         void update_boundary(BoundaryVals bv);
 
     protected:
-    Size N_overlap, N_nonoverlap;
+    Size N_overlap;                 //*< @brief the number of nodes in the subdomain including overlap
+    Size N_nonoverlap;              //*< @brief the number of nodes in the subdomain without overlap
 };
 
 
@@ -113,9 +207,13 @@ template<Dimension dim> class DiscreteSolver : public PDESolver<dim>{};
 template<> class DiscreteSolver<Line> : public PDESolver<Line> {
 
     public:
+        /**
+         * @brief A status representing the state of the solver
+         * @see Types
+         */
         Status status;
-        Real iter_diff;
-        Index iter;
+        Real iter_diff;             //*< @brief the residual computed as \f$\max_i(||u_i^{k+1}-u_i^k||)\f$
+        Index iter;                 //*< @brief the iteration reached so far
 
         DiscreteSolver(const PDEParams &pdep, const SchwarzParams &sp, const SolverParams &solver_params, const Real h);
         ~DiscreteSolver() = default;
@@ -127,19 +225,31 @@ template<> class DiscreteSolver<Line> : public PDESolver<Line> {
          */
         void solve();
 
+        /**
+         * @brief getter for the solution vector
+         * @return the solution at the current iteration
+         */
         Vector get_solution() const;
 
         /**
-         * @brief TODO
+         * @brief prints the solution to a vtk file
+         * Checks that the file is in the right format and prints the current solution to
+         * a .vtk file readable by paraview
+         * @see https://www.paraview.org/
          */
         void print_to_file(std::filesystem::path output);
+        /**
+         * @brief prints the solution to standard output
+         * @see https://www.paraview.org/
+         */
         void print() const;
 
     protected:
-        int max_iter;
-        std::vector<SubdomainSolver<Line>> subdomain_solvers;
-        Vector u_k, u_next;
-        Real eps;
+        int max_iter;                                               //*< @brief the maximum nuber of iteration before stopping
+        std::vector<SubdomainSolver<Line>> subdomain_solvers;       //*< @see SubdomainSolver
+        Vector u_k;                                                 //*< @brief the currently computed solution
+        Vector u_next;                                              //*< @brief a vector to host the next iteration while it's being computed
+        Real eps;                                                   //*< @brief the tolerance s.t. \f$||\delta^k|| < \epsilon\f$
 
 
     private:
@@ -154,7 +264,7 @@ template<> class DiscreteSolver<Line> : public PDESolver<Line> {
          * constucts $u_{k+1}$
          * @return The global solution vector
          */
-        Vector gather_partial_solutions() const;
+        // Vector gather_partial_solutions() const;
 
         /**
          * Computes the current boundary conditions for subdomain i
